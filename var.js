9802b1e253cee9473c1a034ac6125b4d14802a4e
sharjis <sharjis.khan07@gmail.com>
2020-12-11 02:28:33 +0600

    second

---
+++ var.js
@@ -0,0 +1,16 @@
+//var promise = "bekarAr KArke Hume na jayiye"
+//console.log(promise.toLowerCase()); eivabe lower case kore shompurno
+
+//.split
+// .indexof diye khuje ber kore kono word ache kina
+//parseFloat
+//parseInt
+
+var num1= 10;
+var num2= '15.5';
+//num2=+num2; convert kore sting to number
+//num1=''+num1 number theke string e convert kore
+// .toFixed diye doshomiker por koto ghor print korbe sheta bole deya hoy
+
+var a = num1+num2;
+console.log(a);
