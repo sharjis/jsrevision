42640e78b695d7a19d8523d2b26b2734ace37b8c
sharjis <sharjis.khan07@gmail.com>
2020-12-11 03:02:34 +0600

    round-floor-ceil

--- var.js
+++ var.js
@@ -6,11 +6,30 @@
 //parseFloat
 //parseInt
 
-var num1= 10;
+/*var num1= 10;
 var num2= '15.5';
 //num2=+num2; convert kore sting to number
 //num1=''+num1 number theke string e convert kore
 // .toFixed diye doshomiker por koto ghor print korbe sheta bole deya hoy
 
 var a = num1+num2;
-console.log(a);
+console.log(a);*/
+//i=i+1 or simply i++
+//i-- mane i=i-1
+// var price1=100;
+// var price2=234;
+// var total=price1+price2;
+// console.log(total);
+
+//gap between 2 string:
+/*var one= 'Sabbir';
+var two= 'Khan';
+var result = one + ' ' +two;//ekhane space ache
+console.log(result); */
+// Math.abs(ekhane parameter boshbe) hocche absoloute banaya dey
+//Math.round(ekhane boshbe) 
+//Math.ceil() diye beshi mantai show korbe
+//Math.floor() diye doshomik bad diye dey
+
+
+
